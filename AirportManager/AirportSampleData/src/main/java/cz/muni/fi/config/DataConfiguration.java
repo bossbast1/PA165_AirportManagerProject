9a1af291d8869e381757bfc4a93bfbd07ee1336b
bossbast1 <bast1@centrum.sk>
2015-12-15 18:25:18 +0100

    Pridane konfiguracie pre SampleData a WEB aplikaciu Prvotny kontroler pre stewarda

---
+++ AirportManager/AirportSampleData/src/main/java/cz/muni/fi/config/DataConfiguration.java
@@ -0,0 +1,37 @@
+package cz.muni.fi.config;
+
+import cz.muni.fi.airportservicelayer.config.ServiceTestConfiguration;
+import java.text.ParseException;
+import java.util.logging.Level;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author jkaspar
+ */
+@Configuration
+@Import(ServiceTestConfiguration.class)
+@ComponentScan(basePackageClasses = {DataLoadingFacade.class})
+public class DataConfiguration {
+
+    private final static Logger log = LoggerFactory.getLogger(DataConfiguration.class);
+
+    @Autowired
+    private DataLoadingFacade dataLoadingFacade;
+
+    @PostConstruct
+    public void LoadData() {
+        log.trace("Populate database");
+        try {
+            dataLoadingFacade.loadData();
+        } catch (ParseException ex) {
+            log.error("Population Failed on Date formatter parse", ex);
+        }
+    }
+}
---
+++ AirportManager/AirportWeb/src/main/java/cz/muni/fi/mvc/config/WebAppConfig.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.mvc.config;
+
+import cz.muni.fi.config.DataConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ *
+ * @author Sebastian
+ */
+@Configuration
+@EnableWebMvc
+@Import(DataConfiguration.class)
+@ComponentScan(basePackages = "cz.muni.fi.mvc.controllers")
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+}
